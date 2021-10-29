66b0d8b80dcf9f18e248e17721f0b364d5d56a00
José Velasco <79725102+jovelasc@users.noreply.github.com>
2021-10-29 17:32:55 +0200

    Initial commit

---
+++ yujo/src/controllers/FormScreenController.js
@@ -0,0 +1,8 @@
+import '../styles/FormScreen.style.css';
+import FormScreen from "../views/FormScreen";
+
+function FormScreenController() {
+  return <FormScreen />;
+}
+
+export default FormScreenController;
